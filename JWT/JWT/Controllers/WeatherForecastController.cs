7137d47b745e36d0ca4a15b5aa1516ec1c56608d
huudangphan <64909707+huudangphan@users.noreply.github.com>
2021-04-16 17:12:30 +0700

    d

--- JWT/JWT/Controllers/WeatherForecastController.cs
+++ JWT/JWT/Controllers/WeatherForecastController.cs
@@ -26,11 +26,9 @@ namespace JWT.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;  
-                                  
+            _logger = logger;                                   
         }
         [HttpGet]        
         public IEnumerable<WeatherForecast> Get()
